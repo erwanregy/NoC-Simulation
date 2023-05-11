416ed47d252db84bdb167aa031ababa5597021f6
Erwan Régy <81965251+erwanregy@users.noreply.github.com>
2023-05-11 17:58:24 +0100

    Updated script and created README

---
+++ README.md
@@ -0,0 +1,38 @@
+# Noxim Simulation Runner
+
+This script automates running Noxim simulations for different network configurations and parameters. It supports running simulations for parts A, B, and C, with specific modifications for part C.
+
+## Requirements
+
+- Python 3.7 or higher
+- Noxim simulator installed and compiled
+
+## Usage
+
+To run the script, open a terminal and navigate to the directory containing the script. Then, run the following command:
+
+```sh
+python run.py <parts> --modifications <modifications>
+```
+
+Replace `<parts>` with the desired part to run (`a`, `b`, `c`, or `all`), and, optionally, `<modifications>` with the desired modification within part C (`buffer_size`, `hotspot_distribution`, `buffer_selection_strategy`, `routing_algorithm`, or `all`).
+
+For example, to run parts A and B:
+
+```sh
+python run.py a b
+```
+
+To run part C with the buffer size modifications:
+
+```sh
+python run.py c --modifications buffer_size
+```
+
+## Output
+
+The script will create and store results in CSV format in each part's respective directory. The CSV files will contain the relevant metrics for each configuration or parameter value.
+
+## Notes
+
+Before running the script, make sure to set the correct path to the Noxim simulator by updating the `noxim_path` variable at the beginning of the script.
--- run.py
+++ run.py
@@ -1,18 +1,33 @@
 from pathlib import Path
-import subprocess, re
+import subprocess, re, argparse, time
 
-noxim_path: Path = Path("noxim")
+noxim_path: Path = Path("~/noxim")
 
 
 class Variable:
     name: str
-    argument_name: str
     values: list
-    
-    def __init__(self, name: str, argument_name: str, values: list) -> None:
+    arguments: list[str]
+
+    def __init__(self, name: str, values: list, arguments: list[str]) -> None:
         self.name = name
-        self.argument_name = argument_name
         self.values = values
+        self.arguments = arguments
+
+
+data_patterns = [
+    r"(?<=Total received packets: )[0-9]+",
+    r"(?<=Total received flits: )[0-9]+",
+    r"(?<=Received/Ideal flits Ratio: )[0-9.]+",
+    r"(?<=Average wireless utilization: )[0-9]+",
+    r"(?<=Global average delay \(cycles\): )[0-9.]+",
+    r"(?<=Max delay \(cycles\): )[0-9]+",
+    r"(?<=Network throughput \(flits/cycle\): )[0-9.]+",
+    r"(?<=Average IP throughput \(flits/cycle/IP\): )[0-9.]+",
+    r"(?<=Total energy \(J\): )[0-9.e-]+",
+    r"(?<=Dynamic energy \(J\): )[0-9.e-]+",
+    r"(?<=Static energy \(J\): )[0-9.e-]+",
+]
 
 
 def run(directory: Path, variable: Variable) -> None:
@@ -20,45 +35,232 @@ def run(directory: Path, variable: Variable) -> None:
         results_file.write(
             f"{variable.name},Total received packets,Total received flits,Received/Ideal flits ratio,Average wireless utilization,Global average delay (cycles),Max delay (cycles),Network throughput (flits/cycle),Average IP throughput (flits/cycle/IP),Total energy (J),Dynamic energy (J),Static energy (J)\n"
         )
+        results_file.flush()
 
-        for iteration, value in enumerate(variable.values):
-            progress = iteration / len(variable.values) * 10
-            print(f"[{'#' * int(progress)}{' ' * int(10 - progress)}] {progress * 10:.2f}", end='\r')
-
-            command = f"./{noxim_path.joinpath('bin', 'noxim')} -power {noxim_path}/bin/power.yaml -config {noxim_path.joinpath('configuration.yaml')} -{variable.argument_name} {value}"
-            output = subprocess.check_output(
-                command, shell=True, stderr=subprocess.DEVNULL, text=True
+        for iteration, (value, argument) in enumerate(
+            zip(variable.values, variable.arguments)
+        ):
+            progress = iteration / (len(variable.values) - 1) * 20
+            print(
+                f"\r[{'=' * int(progress)}{' ' * (20 - int(progress))}] {int(progress * 5)}% ",
+                end="",
             )
 
-            data_points = {
-                "total_received_packets": r"(?<=Total received packets: )[0-9]+",
-                "total_received_flits": r"(?<=Total received flits: )[0-9]+",
-                "received_ideal_flits_ratio": r"(?<=Received/Ideal flits Ratio: )[0-9.]+",
-                "average_wireless_utilization": r"(?<=Average wireless utilization: )[0-9]+",
-                "global_average_delay": r"(?<=Global average delay \(cycles\): )[0-9.]+",
-                "max_delay": r"(?<=Max delay \(cycles\): )[0-9]+",
-                "network_throughput": r"(?<=Network throughput \(flits/cycle\): )[0-9.]+",
-                "average_IP_throughput": r"(?<=Average IP throughput \(flits/cycle/IP\): )[0-9.]+",
-                "total_energy": r"(?<=Total energy \(J\): )[0-9.e-]+",
-                "dynamic_energy": r"(?<=Dynamic energy \(J\): )[0-9.e-]+",
-                "static_energy": r"(?<=Static energy \(J\): )[0-9.e-]+",
-            }
+            command = f"{noxim_path.joinpath('bin', 'noxim')} -power {noxim_path.joinpath('bin', 'power.yaml')} -config '{directory.joinpath('configuration.yaml')}' {argument}"
+            output = subprocess.check_output(
+                command, shell=True, text=True, stderr=subprocess.DEVNULL
+            )
 
-            extracted_data = [
-                value,
-                *[re.search(regex, output).group() for regex in data_points.values()],
-            ]
+            data = [str(value)]
+            for pattern in data_patterns:
+                match = re.search(pattern, output)
+                if match:
+                    data.append(match.group())
+                else:
+                    data.append("")
 
-            results_file.write(",".join(str(val) for val in extracted_data) + "\n")
+            results_file.write(",".join(data_point for data_point in data) + "\n")
             results_file.flush()
+    print()
+
+def part_a():
+    print("Part A")
+
+    import numpy as np
+
+    start = 0.001
+    step = 0.001
+    end = 0.1
+
+    values = np.arange(start, end + step, step).round(3).tolist()
+    
+    arguments = [f"-pir {value} poisson" for value in values]
+
+    packet_injection_rate = Variable(
+        "Packet injection rate",
+        values,
+        arguments
+    )
+
+    run(Path.cwd().joinpath("Part A"), packet_injection_rate)
+
+
+def part_b():
+    print("Part B")
+    
+    import numpy as np
+
+    start = 0.001
+    step = 0.001
+    end = 1
+
+    values = np.arange(start, end + step, step).round(3).tolist()
+
+    arguments = [
+        f"-pir {value} poisson -hs 0 0.05 -hs 1 0.05 -hs 2 0.05 -hs 3 0.05 -hs 8 0.05 -hs 9 0.05 -hs 10 0.05 -hs 11 0.05"
+        for value in values
+    ]
+
+    packet_injection_rate = Variable(
+        "Packet injection rate",
+        values,
+        arguments
+    )
+
+    run(Path.cwd().joinpath("Part B"), packet_injection_rate)
+
+
+def part_c_buffer_size():
+    print("Part C: Buffer size")
+    values = [2 ** size for size in range(1, 7)]
+
+    arguments = [
+        f"-buffer {value} -pir 0.01 poisson -hs 0 0.05 -hs 1 0.05 -hs 2 0.05 -hs 3 0.05 -hs 8 0.05 -hs 9 0.05 -hs 10 0.05 -hs 11 0.05"
+        for value in values
+    ]
+
+    buffer_size = Variable(
+        "Buffer size",
+        values,
+        arguments,
+    )
+
+    run(Path.cwd().joinpath("Part C", "Buffer Size"), buffer_size)
+
+
+def part_c_routing_algorithm():
+    print("Part C: Routing Algorithm")
+    
+    values = [
+        "WEST_FIRST",
+        "NORTH_LAST",
+        "NEGATIVE_FIRST",
+        "ODD_EVEN",
+        "DYAD",
+    ]
+
+    arguments = [
+        f"-routing {value + ' 0.6' if value == 'DYAD' else value} -pir 0.01 poisson -hs 0 0.05 -hs 1 0.05 -hs 2 0.05 -hs 3 0.05 -hs 8 0.05 -hs 9 0.05 -hs 10 0.05 -hs 11 0.05"
+        for value in values
+    ]
+    
+    routing_algorithm = Variable(
+        "Routing algorithm",
+        values,
+        arguments,
+    )
+    
+    run(Path.cwd().joinpath("Part C", "Routing Algorithm"), routing_algorithm)
+
+
+def part_c_buffer_selection_strategy():
+    print("Part C: Buffer selection strategy")
+    values = [
+        "RANDOM",
+        "BUFFER_LEVEL",
+        "NOP",
+    ]
+
+    arguments = [
+        f"-sel {value} -pir 0.1 poisson -hs 0 0.05 -hs 1 0.05 -hs 2 0.05 -hs 3 0.05 -hs 8 0.05 -hs 9 0.05 -hs 10 0.05 -hs 11 0.05"
+        for value in values
+    ]
+
+    buffer_selection_strategy = Variable(
+        "Buffer selection strategy",
+        values,
+        arguments,
+    )
+
+    run(
+        Path.cwd().joinpath("Part C", "Buffer Selection Strategy"),
+        buffer_selection_strategy,
+    )
+
+
+def part_c_hotspot_distribution():
+    print("Part C: Hotspot distribution")
+    
+    values = [
+        "DEFAULT",
+        "EDGE",
+        "CENTER",
+        "SPREAD",
+    ]
+
+    arguments = [
+        "-hs 0 0.05 -hs 1 0.05 -hs 2 0.05 -hs 3 0.05 -hs 8 0.05 -hs 9 0.05 -hs 10 0.05 -hs 11",
+        "-hs 0 0.05 -hs 3 0.05 -hs 7 0.05 -hs 31 0.05 -hs 32 0.05 -hs 56 0.05 -hs 60 0.05 -hs 63"
+        "-hs 26 0.05 -hs 27 0.05 -hs 28 0.05 -hs 29 0.05 -hs 34 0.05 -hs 35 0.05 -hs 36 0.05 -hs 37"
+        "-hs 0 0.05 -hs 7 0.05 -hs 18 0.05 -hs 21 0.05 -hs 42 0.05 -hs 45 0.05 -hs 56 0.05 -hs 63",
+    ]
+
+    hotspot_distribution = Variable(
+        "Hotspot distribution",
+        values,
+        arguments,
+    )
+
+    run(Path.cwd().joinpath("Part C", "Hotspot Distribution"), hotspot_distribution)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run Noxim simulations")
+
+    parser.add_argument(
+        choices=["a", "b", "c", "all"],
+        help="Part to run",
+    )
+    parser.add_argument(
+        "-m",
+        "--modifications",
+        choices=[
+            "buffer_size",
+            "routing_algorithm",
+            "buffer_selection_strategy",
+            "hotspot_distribution",
+            "all",
+        ],
+        help="Part C modification to run",
+        nargs="+",
+    )
+
+    args = parser.parse_args()
+
+    start_time = time.time()
+    
+    if "a" in args.parts:
+        part_a()
+    if "b" in args.parts:
+        part_b()
+    if "c" in args.parts:
+        if "buffer_size" in args.modifications:
+            part_c_buffer_size()
+        if "buffer_selection_strategy" in args.modifications:
+            part_c_buffer_selection_strategy()
+        if "routing_algorithm" in args.modifications:
+            part_c_routing_algorithm()
+        if "hotspot_distribution" in args.modifications:
+            part_c_hotspot_distribution()
+        elif "all" in args.modifications:
+            part_c_buffer_size()
+            part_c_routing_algorithm()
+            part_c_buffer_selection_strategy()
+            part_c_hotspot_distribution()
+        else:
+            raise ValueError("No modifications specified")
+    elif "all" in args.parts:
+        part_a()
+        part_b()
+        part_c_buffer_size()
+        part_c_routing_algorithm()
+        part_c_buffer_selection_strategy()
+        part_c_hotspot_distribution()
+    else:
+        raise ValueError("No parts specified")
+        
+    print(f"Total time taken: {time.time() - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
-    import numpy as np
-    
-    start = 0.001
-    step = 0.001
-    
-    variable = Variable("Packet injection rate", "pir", list(np.arange(start, 0.1, step)))
-
-    run(Path.cwd(), variable)
+    main()
